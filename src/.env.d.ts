ff9604219453d5898768751f3a4750a12dc3ad9d
vriskaserket51 <serketvriska51@gmail.com>
2022-10-21 08:40:31 +0900

    Edit env file

--- src/.env.d.ts
+++ src/.env.d.ts
@@ -1,5 +1,6 @@
 interface ImportMeta {
     readonly env: {
+        MODE: string;
         readonly VITE_WEB_TITLE: string;
         readonly VITE_WEB_PREFIX: string;
         readonly VITE_API_PREFIX: string;
