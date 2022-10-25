914fc3587678a7092de2a19e5fd80fe45450b181
vriskaserket51 <serketvriska51@gmail.com>
2022-10-25 20:05:23 +0900

    Sync Idbot page

--- src/pages/chat/Idbot.tsx
+++ src/pages/chat/Idbot.tsx
@@ -1,8 +1,8 @@
 import { Box, Divider, Paper, Typography } from "@mui/material";
-import axios from "axios";
 import { ChatController, MuiChat } from "../../components/chat-ui-react";
 import React from "react";
 import "../../styles/ChatStyle.css";
+import { getIdbotChat } from "../../utils/Api";
 
 const BOT_NAME = "이디봇";
 
@@ -26,12 +26,12 @@ function Idbot() {
             });
         };
         // Chat content is displayed using ChatController
-        await addBotMessage(`안녕하세요! ${BOT_NAME}이에요. 저는 다음과 같은 기능을 할 수 있어요. 다음 예시문장으로 제 기능을 테스트 해 보세요!<br>
-        - 과목별 교사 확인: 물리학 과목 선생님은 누가 있어?<br>
-        - 급식표: 오늘 급식 알려줘<br>
-        - 학사일정: 학사일정 알려줘<br>
-        - 생활지도 교사 확인: 생활지도쌤 누구야?<br>
-        - 상수표: 상수표 보여줘<br>
+        await addBotMessage(`안녕하세요! ${BOT_NAME}이에요. 저는 다음과 같은 기능을 할 수 있어요. 다음 예시문장으로 제 기능을 테스트 해 보세요!<br />
+        - 과목별 교사 확인: 물리학 과목 선생님은 누가 있어?<br />
+        - 급식표: 오늘 급식 알려줘<br />
+        - 학사일정: 학사일정 알려줘<br />
+        - 생활지도 교사 확인: 생활지도쌤 누구야?<br />
+        - 상수표: 상수표 보여줘<br />
         - 그 외: 조졸조진 어떻게 해?/점심시간은 몇시야?/특별실 어떻게 써?/학활실 이디저디에서 써도 돼?`);
         chatCtl.setActionRequest(
             {
@@ -41,9 +41,6 @@ function Idbot() {
                 placeholder: "메시지를 입력해 주세요.",
             },
             (response) => {
-                // console.log({
-                // chat_input: response.value
-                // })
                 chatCtl.addMessage({
                     type: "text",
                     content: response.value,
@@ -51,14 +48,9 @@ function Idbot() {
                     avatar: "https://cdn-icons-png.flaticon.com/512/1177/1177594.png",
                     username: "나",
                 });
-                axios
-                    .get(
-                        `https://api.이디저디.com/v1/idbot?chat_input=${response.value}`
-                    )
-                    .then((res) => {
-                        // console.log(res.data)
-                        addBotMessage(res.data.chat_output);
-                    });
+                getIdbotChat({ chatInput: response.value }, (result) => {
+                    addBotMessage(result.chatOutput);
+                });
             }
         );
     }, [chatCtl]);
