a9f4de5654ed3f0231062c20865810c6558a1fec
vriskaserket51 <serketvriska51@gmail.com>
2022-10-26 23:24:41 +0900

    Add current dinner highlight

--- src/components/panel/MealPanel.tsx
+++ src/components/panel/MealPanel.tsx
@@ -2,7 +2,57 @@ import { GetMealResponse } from "@common-jshs/menkakusitsu-lib/v1";
 import React from "react";
 import { getMeal } from "../../utils/Api";
 import dayjs from "dayjs";
-import { Box, Divider, Paper, Typography } from "@mui/material";
+import { Box, Divider, Paper, SxProps, Theme, Typography } from "@mui/material";
+
+interface MealInfoProps {
+    type: "breakfast" | "lunch" | "dinner";
+    meals?: string[];
+}
+
+const MealInfo = (props: MealInfoProps) => {
+    const currentHour = dayjs().hour();
+    let mealName: string = "";
+    let highlitedMeal: boolean = false;
+    switch (props.type) {
+        case "breakfast":
+            mealName = "아침";
+            highlitedMeal = currentHour >= 19;
+            break;
+        case "lunch":
+            mealName = "점심";
+            highlitedMeal = currentHour >= 13 && currentHour < 19;
+            break;
+        case "dinner":
+            mealName = "저녁";
+            highlitedMeal = currentHour < 13;
+            break;
+    }
+    const sx: SxProps<Theme> = highlitedMeal
+        ? {
+              display: "block",
+              padding: "50px 30px 30px 30px",
+              borderRadius: "1px",
+              borderStyle: "solid none none none",
+              borderColor: "primary.main",
+              borderWidth: "2px",
+          }
+        : {
+              display: "block",
+              padding: "50px 30px 30px 30px",
+          };
+    return (
+        <Box sx={sx}>
+            <Typography variant="h5">{mealName}</Typography>
+            {props.meals && props.meals.map((meal) => {
+                return (
+                    <Typography key={meal} variant="h6">
+                        - {meal}
+                    </Typography>
+                );
+            })}
+        </Box>
+    );
+};
 
 function MealPanel() {
     const [mealInfo, setMealInfo] = React.useState<GetMealResponse | null>(
@@ -29,41 +79,11 @@ function MealPanel() {
                         justifyContent: "space-around",
                     }}
                 >
-                    <Box
-                        sx={{
-                            display: "block",
-                            padding: "50px 30px 30px 30px",
-                        }}
-                    >
-                        <Typography variant="h5">점심</Typography>
-                        {mealInfo?.lunch.meals.map((meal) => {
-                            return <Typography key={meal} variant="h6">- {meal}</Typography>;
-                        })}
-                    </Box>
+                    <MealInfo type="lunch" meals={mealInfo?.lunch.meals} />
                     <Divider orientation="vertical" flexItem />
-                    <Box
-                        sx={{
-                            display: "block",
-                            padding: "50px 30px 30px 30px",
-                        }}
-                    >
-                        <Typography variant="h5">저녁</Typography>
-                        {mealInfo?.dinner.meals.map((meal) => {
-                            return <Typography key={meal} variant="h6">- {meal}</Typography>;
-                        })}
-                    </Box>
+                    <MealInfo type="dinner" meals={mealInfo?.dinner.meals} />
                     <Divider orientation="vertical" flexItem />
-                    <Box
-                        sx={{
-                            display: "block",
-                            padding: "50px 30px 30px 30px",
-                        }}
-                    >
-                        <Typography variant="h5">아침</Typography>
-                        {mealInfo?.breakfast.meals.map((meal) => {
-                            return <Typography key={meal} variant="h6">- {meal}</Typography>;
-                        })}
-                    </Box>
+                    <MealInfo type="breakfast" meals={mealInfo?.breakfast.meals} />
                 </Paper>
             </Box>
         </React.Fragment>
