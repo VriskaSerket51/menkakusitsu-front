e9882bea99ee4cb20dbbee2e4fccf4b8fd0d69e4
vriskaserket51 <serketvriska51@gmail.com>
2022-11-15 12:02:19 +0900

    정규화

--- src/pages/Construct.tsx
+++ src/pages/Construct.tsx
@@ -2,11 +2,11 @@ import { Typography } from "@mui/material";
 import React from "react";
 
 function Construct() {
-    return <React.Fragment>
-        <Typography>
-            공사 중 ㅎㅎ
-        </Typography>
-    </React.Fragment>;
+    return (
+        <React.Fragment>
+            <Typography>공사 중 ㅎㅎ</Typography>
+        </React.Fragment>
+    );
 }
 
 export default Construct;
