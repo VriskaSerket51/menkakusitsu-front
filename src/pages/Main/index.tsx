315d1d6b19296ddf25ef51d703a996e3b3c1a3be
vriskaserket51 <serketvriska51@gmail.com>
2022-11-28 11:12:21 +0900

    로그인 패널 좀 바꿈

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -41,10 +41,10 @@ function Main() {
                 }}
             >
                 <Grid container spacing={3}>
-                    <Grid item xs={8}>
+                    <Grid item xs={8.5}>
                         <SpecialroomInfoPanel />
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={3.5}>
                         <LoginPanel />
                     </Grid>
                     <Grid item xs={12}>
