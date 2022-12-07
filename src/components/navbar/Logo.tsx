1cd0346dca9b3cbf61198f251f89117e819dd59d
vriskaserket51 <serketvriska51@gmail.com>
2022-12-07 22:26:31 +0900

    내비 로고도 루트 이용

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,33 +1,43 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import { Box, Link } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 function Logo() {
+    const navigate = useNavigate();
+
     return (
-            <Box
-                style={{
-                    display: "inline-block",
-                    marginLeft: "64px",
+        <Box
+            style={{
+                display: "inline-block",
+                marginLeft: "64px",
+            }}
+        >
+            <Link
+                color="inherit"
+                underline="none"
+                onClick={() => {
+                    navigate("/");
                 }}
+                sx={{cursor: "pointer"}}
             >
-                <Link href="/" color="inherit" underline="none">
-                    <Typography
-                        sx={{ fontFamily: "DesignHouseB" }}
-                        variant="h4"
-                        color="primary.main"
-                    >
-                        {import.meta.env.VITE_WEB_TITLE}
-                    </Typography>
-                    <Box
-                        sx={{
-                            marginTop: "-10px",
-                            backgroundColor: "primary.light",
-                            height: "4px",
-                            borderRadius: "10px",
-                        }}
-                    />
-                </Link>
-            </Box>
+                <Typography
+                    sx={{ fontFamily: "DesignHouseB" }}
+                    variant="h4"
+                    color="primary.main"
+                >
+                    {import.meta.env.VITE_WEB_TITLE}
+                </Typography>
+                <Box
+                    sx={{
+                        marginTop: "-10px",
+                        backgroundColor: "primary.light",
+                        height: "4px",
+                        borderRadius: "10px",
+                    }}
+                />
+            </Link>
+        </Box>
     );
 }
 
