4e486b3ae0136eca23449a0541e0aa11f2f94247
vriskaserket51 <serketvriska51@gmail.com>
2022-12-07 22:42:17 +0900

    백업

---
+++ src/pages/index(old).tsx
@@ -0,0 +1,27 @@
+export { default as Main } from "./Main";
+
+export { default as AttendanceDownload } from "./attendance/Download";
+export { default as AttendanceInfo } from "./attendance/Info";
+
+export { default as BbsCreate } from "./bbs/Create";
+export { default as BbsEdit} from "./bbs/Edit";
+export { default as BbsList } from "./bbs/List";
+export { default as BbsPost } from "./bbs/Post";
+
+export { default as ChatIdbot } from "./chat/Idbot";
+export { default as ChatRandom } from "./chat/RandomChat";
+
+export { default as PlaySnake } from "./playground/snake/SnakeGame";
+
+export { default as SpecialroomApply } from "./specialroom/Apply";
+export { default as SpecialroomStatus } from "./specialroom/Status";
+export { default as SpecialroomManagement } from "./specialroom/Management";
+
+export { default as SurveyCreate } from "./survey/Create";
+
+export { default as About } from "./About";
+
+export { default as Setting } from "./setting";
+
+export { default as NotFound } from "./NotFound";
+export { default as Construct } from "./Construct";
