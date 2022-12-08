d96e21482d4b97a8bddf81abcbdb7198eceadf1b
vriskaserket51 <serketvriska51@gmail.com>
2022-12-08 14:10:21 +0900

    router loading 추기

---
+++ src/components/route-loading/DefaultLoadingScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function DefaultLoadingScreen() {
+    return (
+        <div
+            style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                backgroundColor: "white",
+            }}
+        />
+    );
+}
