6a419a50326ec402d733ae04e8e8abba25d3dd41
vriskaserket51 <serketvriska51@gmail.com>
2023-01-04 06:57:26 +0900

    폴더 이름 직관적으로 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export * from "./button";
 export * from "./navbar";
 export * from "./panel";
-export * from "./layout/ThemeLayout";
+export * from "./router/ThemeLayout";
 export * from "./popup";
 export * from "./snackbar";
