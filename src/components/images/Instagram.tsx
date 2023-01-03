2eea5b9f876dc49153e58297af145fa635b57fdd
vriskaserket51 <serketvriska51@gmail.com>
2023-01-04 08:25:21 +0900

    인스타 로고 추가

---
+++ src/components/images/Instagram.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import instagramIcon from "../../assets/img/instagram_logo.svg";
+
+export function InstagramLogo() {
+    return <img src={instagramIcon} style={{ height: "1.5em" }} />;
+}
