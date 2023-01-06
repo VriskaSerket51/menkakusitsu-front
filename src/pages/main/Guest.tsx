520c804ff14dd327e16af34a489e78009a30619c
vriskaserket51 <serketvriska51@gmail.com>
2023-01-06 22:17:58 +0900

    게스트 화면 파일 분리

---
+++ src/pages/main/Guest.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import { SpecialroomInfoPanel, MealPanel, LoginPanel } from "../../components";
+import { Container, Typography, Grid, Link } from "@mui/material";
+import {
+    setHeaderActive,
+} from "../../components/router/RouteWrapper";
+
+export default function Guest() {
+    useEffect(() => {
+        setHeaderActive(false);
+        return () => {
+            setHeaderActive(true);
+        };
+    }, []);
+
+    return (
+        <React.Fragment>
+            <Typography
+                variant="h3"
+                noWrap
+                align="center"
+                sx={{
+                    mr: 2,
+                    fontWeight: 500,
+                    fontFamily: "DesignHouseB",
+                    margin: "50px auto 30px",
+                }}
+            >
+                <Link href="/" underline="none" color="primary.main">
+                    {import.meta.env.VITE_WEB_TITLE}
+                </Link>
+            </Typography>
+            <Container
+                maxWidth="xl"
+                sx={{
+                    margin: "30px auto 50px",
+                }}
+            >
+                <Grid container spacing={3}>
+                    <Grid item xs={8.5}>
+                        <SpecialroomInfoPanel />
+                    </Grid>
+                    <Grid item xs={3.5}>
+                        <LoginPanel />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <MealPanel />
+                    </Grid>
+                </Grid>
+            </Container>
+        </React.Fragment>
+    );
+}
