fa923f41306af6d4af2c2308a4c06bd3176fce9b
vriskaserket51 <serketvriska51@gmail.com>
2023-01-06 22:19:01 +0900

    안 쓰는 함수 제거

--- src/utils/AuthManager.ts
+++ src/utils/AuthManager.ts
@@ -1,34 +1,10 @@
 import axios from "axios";
-import { openConfirmDialog } from "../components/popup";
-import { TITLE } from "./Constant";
 import {
-    BackendResponse,
     clearTokens,
     parseJWT,
     redirectToHome,
 } from "./Utility";
 import { PostRefreshResponse } from "@common-jshs/menkakusitsu-lib/v1";
-
-export const checkTokenError = (resp: BackendResponse) => {
-    const result = resp.data;
-    if (result.status >= 0) {
-        return resp;
-    }
-    if (result.status === -1972) {
-        openConfirmDialog(TITLE.Alert, "세션이 만료됐습니다!", () => {
-            clearTokens();
-            redirectToHome();
-        });
-        throw new Error(result.message);
-    } else if (result.status === -1973) {
-        openConfirmDialog(TITLE.Alert, "손상된 토큰입니다!", () => {
-            clearTokens();
-            redirectToHome();
-        });
-        throw new Error(result.message);
-    }
-    return resp;
-};
 
 export const checkTokenExpiration = async (accessToken: string) => {
     const parsedJWT = parseJWT(accessToken);
