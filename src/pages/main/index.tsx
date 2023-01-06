245f564a18027a2afb41f0385004fb64b56638d9
vriskaserket51 <serketvriska51@gmail.com>
2023-01-06 22:18:54 +0900

    게스트 페이지 파일 분리

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,70 +1,23 @@
-import React, { useEffect } from "react";
-import { SpecialroomInfoPanel, MealPanel, LoginPanel } from "../../components";
-import { Container, Typography, Grid, Link } from "@mui/material";
-import { getUserInfo } from "../../utils/Utility";
+import React from "react";
+import { getPermissionLevel, Permission } from "../../utils/Utility";
 import Teacher from "./Teacher";
 import Student from "./Student";
-import {
-    setFooterActive,
-    setHeaderActive,
-} from "../../components/router/RouteWrapper";
+import Guest from "./Guest";
 
 function Main() {
-    const accessToken = localStorage.getItem("access-token");
-
-    if (accessToken) {
-        const userInfo = getUserInfo();
-        if (userInfo.isTeacher) {
-            return <Teacher />;
-        } else {
+    const permission = getPermissionLevel();
+    switch (permission) {
+        case Permission.Dev:
             return <Student />;
-        }
+        case Permission.Teacher:
+            return <Teacher />;
+        case Permission.Student:
+            return <Student />;
+        case Permission.Guest:
+            return <Guest />;
+        default:
+            return <Guest />;
     }
-
-    useEffect(() => {
-        setHeaderActive(false);
-        return () => {
-            setHeaderActive(true);
-        };
-    }, []);
-
-    return (
-        <React.Fragment>
-            <Typography
-                variant="h3"
-                noWrap
-                align="center"
-                sx={{
-                    mr: 2,
-                    fontWeight: 500,
-                    fontFamily: "DesignHouseB",
-                    margin: "50px auto 30px",
-                }}
-            >
-                <Link href="/" underline="none" color="primary.main">
-                    {import.meta.env.VITE_WEB_TITLE}
-                </Link>
-            </Typography>
-            <Container
-                maxWidth="xl"
-                sx={{
-                    margin: "30px auto 50px",
-                }}
-            >
-                <Grid container spacing={3}>
-                    <Grid item xs={8.5}>
-                        <SpecialroomInfoPanel />
-                    </Grid>
-                    <Grid item xs={3.5}>
-                        <LoginPanel />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <MealPanel />
-                    </Grid>
-                </Grid>
-            </Container>
-        </React.Fragment>
-    );
 }
 
 export default Main;
