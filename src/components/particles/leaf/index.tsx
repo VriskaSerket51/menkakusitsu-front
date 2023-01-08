74b43b15f3ce7d85fc679e97218bf00291a4dee9
vriskaserket51 <serketvriska51@gmail.com>
2023-01-09 01:31:51 +0900

    날짜에 맞는 파티클 추가가

---
+++ src/components/particles/leaf/index.tsx
@@ -0,0 +1,92 @@
+import React, { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { Container, Engine } from "tsparticles-engine";
+
+import leaf from "../../../assets/particles/fall/leaf.png";
+
+export default function LeafParticle() {
+    const particlesInit = useCallback(async (engine: Engine) => {
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(
+        async (container: Container | undefined) => {},
+        []
+    );
+
+    return (
+        <Particles
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                particles: {
+                    shape: {
+                        type: "images",
+                        options: {
+                            images: [
+                                {
+                                    src: leaf,
+                                    width: 128,
+                                    height: 117,
+                                },
+                            ],
+                        },
+                    },
+                    move: {
+                        direction: "bottom-right",
+                        enable: true,
+                        random: false,
+                        straight: false,
+                        speed: 5,
+                    },
+                    rotate: {
+                        value: {
+                            min: 0,
+                            max: 360,
+                        },
+                        direction: "random",
+                        move: true,
+                        animation: {
+                            enable: true,
+                            speed: 10,
+                        },
+                    },
+                    tilt: {
+                        direction: "random",
+                        enable: true,
+                        move: true,
+                        value: {
+                            min: 0,
+                            max: 360,
+                        },
+                        animation: {
+                            enable: true,
+                            speed: 30,
+                        },
+                    },
+                    roll: {
+                        darken: {
+                            enable: true,
+                            value: 25,
+                        },
+                        enable: true,
+                        speed: {
+                            min: 5,
+                            max: 15,
+                        },
+                    },
+                    opacity: {
+                        value: { min: 0.5, max: 0.7 },
+                    },
+                    size: {
+                        value: { min: 15, max: 20 },
+                    },
+                    number: {
+                        max: 15,
+                    },
+                },
+            }}
+        />
+    );
+}
