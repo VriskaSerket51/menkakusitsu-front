e4974bf6bac6acc07f4228d385c3f71ccc15ca59
vriskaserket51 <serketvriska51@gmail.com>
2023-01-10 18:33:30 +0900

    react-router-loading 폐기

--- src/components/route-loading/index.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { LoadingContext, LoadingGetterContext } from "./LoadingContext";
-
-export { default as Route } from "./Route";
-export { default as Routes } from "./Routes";
-
-const useLoadingContext = () => useContext(LoadingContext);
-export { useLoadingContext, LoadingContext, LoadingGetterContext };
