a0e8e86f1fe83c2c3f1036943f2ed91a954b91d2
vriskaserket51 <serketvriska51@gmail.com>
2023-01-12 13:33:19 +0900

    보여지는 게시글 / 댓글 수 조정

--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -5,5 +5,5 @@ export enum DialogTitle {
     Notice = "공지사항",
 }
 
-export const POST_LIST_SIZE = 15;
-export const COMMENT_LIST_SIZE = 15;
+export const POST_LIST_SIZE = 20;
+export const COMMENT_LIST_SIZE = 70;
