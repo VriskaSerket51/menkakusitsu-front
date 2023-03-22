545a89b05d3076029cc2c465a42a113616c01fa4
jordaeni13 <70104240+jordaeni13@users.noreply.github.com>
2023-03-22 22:01:11 +0900

    물의날 베너

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Box, Divider, Stack } from "@mui/material";
 import Banner1 from "../../assets/ebsbanner2023.png";
+import Banner2 from "../../assets/water_banner.jpg";
 import BannerDetails1 from "../../assets/ebsBanner2023Details.pdf";
+
 import { Link } from "react-router-dom";
 
 
 export function Banner() {
+    const BannerDetails2 = "https://me.go.kr/home/web/board/read.do?menuId=10392&boardMasterId=713&boardId=1584980";
     return (
         <React.Fragment>
             <br />
@@ -22,9 +25,14 @@ export function Banner() {
                     spacing={2}
                 >
                     <Link
-                     to={BannerDetails1} 
-                     target="_blank">
-                        <img src={Banner1} alt="W3Schools" width="100%"/ >
+                    to={BannerDetails1} 
+                    target="_blank">
+                        <img src={Banner1} alt="ebs" height="120vh"/ >
+                    </Link>
+                    <Link
+                    to={BannerDetails2} 
+                    target="_blank">
+                        <img src={Banner2} alt="worldwatter" height="120vh"/ >
                     </Link>
                 </Stack>
             </Stack>
